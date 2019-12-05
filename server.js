be644bb65d757a478412446a9d1f91de75a3a834
shankbansal <50798143+shankbansal@users.noreply.github.com>
2019-12-05 12:18:57 +0100

    aligned
    aligned

--- server.js
+++ server.js
@@ -1,37 +1,39 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
-http.createServer((request,response)=>{
-var addr = request.url,
-q = url.parse(addr, true),
-filePath = '';
+http
+  .createServer((request, response) => {
+    var addr = request.url,
+      q = url.parse(addr, true),
+      filePath = "";
 
-  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function(err) {
-  if (err) {
-    console.log(err);
-  }
-    else {
-  console.log('Added to log.');
-  }
-});
+    fs.appendFile(
+      "log.txt",
+      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+      function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log.txt");
+        }
+      }
+    );
 
-
-  if (q.pathname.includes('documentation')) {
-    filePath = (__dirname + '/documentation.html');
-  } else {
-    filePath = 'index.html';
-  }
-
-  fs.readFile(filePath, function(err, data) {
-    if (err) {
-      throw err;
+    if (q.pathname.includes("documentation")) {
+      filePath = __dirname + "/documentation.html";
+    } else {
+      filePath = "index.html";
     }
 
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(data);
-    response.end();
+    fs.readFile(filePath, function(err, data) {
+      if (err) {
+        throw err;
+      }
 
-  });
-
-}).listen(8080);
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(data);
+      response.end();
+    });
+  })
+  .listen(8080);
