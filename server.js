64dd34409f15414c1073313b5b2c50a7190ca58e
shankbansal <50798143+shankbansal@users.noreply.github.com>
2019-12-05 12:31:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
-http
-  .createServer((request, response) => {
+http.createServer((request, response) => {
     var addr = request.url,
       q = url.parse(addr, true),
       filePath = "";
